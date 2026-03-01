928941032ea93bf46ed9ee50712cb1bb104b5150
jan <janalberdriessen@gmail.com>
2026-03-01 12:45:52 +0100

    Auto-sync from OneDrive: 2026-03-01T11:45:52Z

---
+++ python/Main.py
@@ -0,0 +1,30 @@
+from scipy.interpolate import interp1d, CubicSpline
+from scipy.integrate import trapezoid, simpson
+import matplotlib.pyplot as plt
+import pandas as pd
+import numpy as np
+import json
+import random
+import math
+import os
+
+from Class import Ship
+
+file = [98,2,0] #[group, version, subversion]
+
+#constants
+tank3_initial = 70
+hull_thickness = np.array([0.01, 0.012, 0.012]) #[transom, shell, deck]
+BHD_thickness = 0.01
+mass_factor = 2.1
+material_density = 7850
+water_density = 1025
+crane_position = [11,0,6]
+TP_position = [13.2453,0,15]
+TP_mass = 230000
+TP_amount = 4
+jib_length = 40
+jib_angle = 45
+slewing_angle = 180
+
+voorbeeld = Ship(file, crane_position, jib_length, TP_position, TP_amount, hull_thickness, BHD_thickness, tank3_initial, slewing_angle, jib_angle, TP_mass, water_density, material_density, mass_factor)
--- python/antwoorden.py
+++
@@ -1,8 +0,0 @@
-# antwoorden.py
-from calculations import maak_config
-import os
-os.chdir('C:/Delft/Opdrachten INV/test')
-cfg = maak_config()
-
-print("Volume:", round(cfg.bouyant_volume, 2), "m3")
-print("Drijfvermogen:", round(cfg.bouyant_volume * cfg.rho * cfg.g, 0), "N")
